abcad69987a4c214d306bad6f85df4d893f2842f
jeeinkim21 <jeeinkim@csu.fullerton.edu>
2022-02-24 15:03:06 -0800

    testing sample button

--- DateSafe/ViewController.swift
+++ DateSafe/ViewController.swift
@@ -12,8 +12,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        createButton()
     }
 
-
+    func createButton() {
+        let button = UIButton(type: .system)
+        button.frame = CGRect(x:100, y:100, width:100, height:50)
+        button.backgroundColor = .darkGray
+        button.setTitle("Testing", for: .normal)
+        
+        //button.addTarget(self, action: #selector(buttonAction),for: .touchUpInside)
+        
+        self.view.addSubview(button)
+        
+    }
 }
 
