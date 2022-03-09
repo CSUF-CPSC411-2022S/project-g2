4572e9c19f8e08497a046b0dd9f5c71bb2d4ef50
jeeinkim21 <jeeinkim@csu.fullerton.edu>
2022-03-08 21:59:04 -0800

    Updated: button represented by image, toggle effect added

--- DateSafe/ViewController.swift
+++ DateSafe/ViewController.swift
@@ -6,6 +6,62 @@
 //
 
 import UIKit
+import SwiftUI
+
+struct ContentView: View {
+    //This is necessary to have our button image, for views depend on specified states and are set on a case by case basis.
+    @State private var isPressed = false
+    
+    var body: some View {
+        //Checking the state of the isPressed property for a toggle effect.
+        //If button has not been pressed, do nothing. If the button has been pressed, show the new image.
+        Button(action: { }, label: {
+            Image(!isPressed ? "test" : "alternative_test").resizable()
+        })
+            .buttonStyle(PlainButtonStyle()) //remove default highlight
+            .pressAction { //implementation of action from PressAction
+                isPressed = true
+            } kReleased: {
+                isPressed = false
+            }
+            .frame(width: 200, height: 200)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.light)
+    }
+}
+
+//Press Actions for the Button
+struct PressActions: ViewModifier {
+    var kPressed: () -> Void
+    var kReleased: () -> Void
+    func body(content: Content) -> some View {
+        content
+            .simultaneousGesture(
+                DragGesture(minimumDistance: 0)
+                    .onChanged({ _ in
+                        kPressed()
+                    })
+                    .onEnded({ _ in
+                        kReleased()
+                    })
+                )
+    }
+}
+
+extension View {
+    func pressAction(kPressed: @escaping (()-> Void), kReleased: @escaping (() -> Void)) -> some View {
+        modifier(PressActions(kPressed: {
+            kPressed()
+        }, kReleased: {
+            kReleased()
+        }))
+    }
+}
 
 class ViewController: UIViewController {
 
@@ -13,19 +69,19 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        createButton()
+        //createButton()
     }
 
-    func createButton() {
-        let button = UIButton(type: .system)
-        button.frame = CGRect(x:100, y:100, width:100, height:50)
-        button.backgroundColor = .darkGray
-        button.setTitle("Testing", for: .normal)
-        
-        //button.addTarget(self, action: #selector(buttonAction),for: .touchUpInside)
-        
-        self.view.addSubview(button)
-        
-    }
+//    func createButton() {
+//        let button = UIButton(type: .system)
+//        button.frame = CGRect(x:100, y:100, width:100, height:50)
+//        button.backgroundColor = .darkGray
+//        button.setTitle("Testing", for: .normal)
+//
+//        //button.addTarget(self, action: #selector(buttonAction),for: .touchUpInside)
+//
+//        self.view.addSubview(button)
+//
+//    }
 }
 
