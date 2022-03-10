b1b287a8414d557b29b1d3928050a8aff5fb8732
nicholascaro <70615231+nicholascaro@users.noreply.github.com>
2022-03-09 20:08:54 -0800

    Initial Commit

---
+++ DateSmarter/DateSmarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DateSmarterApp.swift
+//  DateSmarter
+//
+//  Created by Nicholas Caro on 3/9/22.
+//
+
+import SwiftUI
+
+@main
+struct DateSmarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
