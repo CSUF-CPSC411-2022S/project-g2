462c0c3b1c94e8a82892973fdaf3ffc011b89c10
Wangmo Tenzing <wtenzing6@gmail.com>
2022-03-09 22:25:31 -0800

    location tracking struct for project check-in 2

--- DateSafe/locationtracking.swift
+++ DateSafe/locationtracking.swift
@@ -5,8 +5,38 @@
 //  Created by Wangmo Tenzing on 2/23/22.
 //
 
-import Foundation
 
-struct LocationTracking {
-    //code
+import Foundation
+import SwiftUI
+
+//use LocationTracking() to call it from ContentView
+
+struct LocationTracking: View {
+    var latitude: Int = 45
+    var longitude: Int = 100
+    var coordinates: String = ""
+    
+    //once our parts are connected, we will
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("Starting Location Tracking...\n")
+            } //HStack
+            HStack {
+                if latitude >= 30 && latitude <= 50 {
+                    if longitude >= 70 && longitude <= 120 {
+                        Text("📍Located detected")
+                    }
+                }
+            }//HStack
+        
+        } //VStack
+    }
+}
+
+struct LocationTracking_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationTracking()
+    }
 }
