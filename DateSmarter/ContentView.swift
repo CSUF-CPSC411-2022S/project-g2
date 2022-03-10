30fa9d9e2c03d8f68d2afcccabf5c73f66033cf3
nicholascaro <70615231+nicholascaro@users.noreply.github.com>
2022-03-09 20:52:24 -0800

    App  is able to send a message to a phone number

--- DateSmarter/ContentView.swift
+++ DateSmarter/ContentView.swift
@@ -8,14 +8,73 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var NumberToMessage = ""
+    @State var Message = ""
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            HStack{
+                Spacer()
+                Button("Im on a bad date") {
+                    sendMessage()
+                }
+                .modifier(DateSmarter.buttonStyle())
+                Spacer()
+                Button("I feel trapped") {
+                    sendMessage()
+                }
+                .modifier(DateSmarter.buttonStyle())
+                Spacer()
+                
+                
+            }
+            HStack{
+                Spacer()
+                Button("I'm being followed") {
+                    sendMessage()
+                }
+                .modifier(DateSmarter.buttonStyle())
+                Spacer()
+                Button("I've been drugged") {
+                    sendMessage()
+                }
+                .modifier(DateSmarter.buttonStyle())
+                Spacer()
+                
+                
+            }
+//            TextField("Enter A Mobile Number: ", text: $NumberToMessage)
+//            TextField("Enter Your Message", text: $Message)
+            
+        }
+    }
+    
+    func sendMessage(){
+        let sms: String = "sms:+16266226475&body=This is a test."
+        
+        let strURL: String = sms.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+        
+        UIApplication.shared.open(URL.init(string: strURL)!, options: [:], completionHandler: nil)
     }
 }
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
+struct buttonStyle: ViewModifier {
+   func body(content: Content) -> some View {
+        content
+           .padding()
+           .accentColor(.white)
+           .background(Color.accentColor)
+           .clipShape(RoundedRectangle(cornerRadius: 14.0, style: .continuous))
+   }
+}
+
+
