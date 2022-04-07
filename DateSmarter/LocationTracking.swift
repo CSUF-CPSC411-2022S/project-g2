b9bec3a071211129dc9484b48c8b45417b8e81b7
Wangmo Tenzing <wtenzing6@gmail.com>
2022-04-07 01:42:05 -0700

    api incorporated, but needs to work with rest of program

--- DateSmarter/LocationTracking.swift
+++ DateSmarter/LocationTracking.swift
@@ -10,21 +10,62 @@ import SwiftUI
 
 //use LocationTracking() to call it from ContentView
 struct LocationTracking {
-    var latitude: Int = 45
-    var longitude: Int = 100
-    var coordinates: String = ""
+    let url = "http://ip-api.com/json/"     //base URL
+    getData(from: url)
+}
+
+
+private func getData(from url: String) {
+//make API request here
+let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { data, response, error in
+    guard let data = data, error == nil else {
+        print("Something went wrong")
+        return
+    }
+    //we now have the data
+    //convert data to Response object
     
-    func trackLocation (_ latitude: Int, at longitude: Int) {
-        if latitude >= 30 && latitude <= 50 {
-            if longitude >= 70 && longitude <= 120 {
-                print("📍Located detected in United States")
-            }
-        }
-        else {
-            print("Location is outside of United States")
-        }
+    var result: Response?
+    do {
+        result = try JSONDecoder().decode(Response.self, from: data)
+    }
+    catch {
+        print("failed to convert \(error.localizedDescription)")
+    }
+    guard let json = result else {
+        return
+    }
+    print(json.query)
+    print(json.status)
+    print(json.country)
+    print(json.countryCode)
+    print(json.region)
+    
+})
+    task.resume()
     }
 }
+
+//response from API call
+//No API key required
+
+struct Response: Codable {
+    let query: String
+    let status: String
+    let country: String
+    let countryCode: String
+    let region: String
+    let regionName: String
+    let city: String
+    let zip: String
+    let lat: Float
+    let lon: Float
+    let timezone: String
+    let isp: String
+    let org: String
+    let AS: String
+}
+
 
 /* struct LocationTracking_Previews: PreviewProvider {
     static var previews: some View {
