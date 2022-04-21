4bc5067872fdd9be164d9ba02b630b98837ee20b
Wangmo Tenzing <wtenzing6@gmail.com>
2022-04-21 01:08:17 -0700

    using lab 6 to guide me in the right direction

--- DateSmarter/LocationTracking.swift
+++ DateSmarter/LocationTracking.swift
@@ -8,6 +8,73 @@
 import Foundation
 import SwiftUI
 
+
+struct Response: Codable {
+    
+    //response from API call
+    //No API key required
+    let query: String
+    let status: String
+    let country: String
+    let countryCode: String
+    let region: String
+    let regionName: String
+    let city: String
+    let zip: String
+    let lat: Float
+    let lon: Float
+    let timezone: String
+    let isp: String
+    let org: String
+    let AS: String
+}
+
+class LocationTracking {
+    
+    //@published variables are only allowed with classes, not structs
+    @Published var translatedQuery = ""
+    @Published var translatedStatus = ""
+    @Published var translatedCountry = ""
+    
+    
+    let ipUrl = "http://ip-api.com/json/"
+    
+    func find(_ searchString: String) {
+        guard searchString != "" else {
+            return
+        }
+    //addingPercentEncoding()
+    if let urlString = ipUrl.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed),
+        let url = URL(string: urlString) {
+        
+        // Creates a task that retrieves the contents of the specified URL, then calls a handler upon completion.
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            DispatchQueue.main.async {
+                let jsonDecoder = JSONDecoder()
+                // Decode the JSON and store in result
+                if let validData = data, let result = try? jsonDecoder.decode(Response.self, from: validData) {
+                    
+                    //testing to see if IP address starts with 1...but right now, it just is testing to see if it is == 1
+                    if result.query == "1" {
+                        self.translatedQuery = result.query
+                    }//if
+                    else {
+                        self.translatedQuery = "No Results Found"
+                    }//else
+                }
+                else {
+                    self.translatedQuery = "No Results Found"
+                }//else
+            }
+            task.resume()
+        }
+    }//let url = URL(string: urlString)
+        
+
+/*
+
 //use LocationTracking() to call it from ContentView
 struct LocationTracking {
     let url = "http://ip-api.com/json/"     //base URL
@@ -65,8 +132,14 @@ struct Response: Codable {
     let org: String
     let AS: String
 }
+ 
+ 
+ */   // end of my actual code
+ 
+ 
+ 
 
-
+//just previews, u can leave it commented out
 /* struct LocationTracking_Previews: PreviewProvider {
     static var previews: some View {
         LocationTracking()
